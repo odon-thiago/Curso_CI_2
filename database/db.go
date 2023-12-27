a9abae1d1ad74f5d7854fa8503ead69f1d4ebb4f
odon-thiago <104784460+odon-thiago@users.noreply.github.com>
2023-12-27 09:04:01 -0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -13,8 +13,7 @@ var (
 	err error
 )
 
-func ConectaComBancoDeDados() {
-	func ConectaComBancoDeDados() {
+func ConectaComBancoDeDados() {	
     	stringDeConexao := "host="+ os.Getenv("HOST") +" user=" + os.Getenv("USER") + " password=" + os.Getenv("PASSWORD") + " dbname=" + os.Getenv("DBNAME") + " port=" + os.Getenv("DBPORT") + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
